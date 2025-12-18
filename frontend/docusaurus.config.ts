436caa016d67f964b3adb16e9a8da74bdbe3433f
TeamTheCoders <new2200527@gmail.com>
2025-12-19 02:00:16 +0500

    Update docusaurus.config.ts

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://TeamTheCoders.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, it is often '/<projectName>/'
-  baseUrl: '/physical-robot/frontend/',
+  baseUrl: '/frontend',
 
   customFields: {
     backendUrl: 'http://localhost:8000', // Default for local development
